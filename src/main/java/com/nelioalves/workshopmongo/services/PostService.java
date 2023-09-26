7d9bacbbe818cda8d6b18a723a13ac8b9face4e1
Lucas Diniz <lucasfonseca108.lf@gmail.com>
2023-09-26 00:36:27 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,17 +1,15 @@
 package com.nelioalves.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
-
 @Service
 public class PostService {
-	
 	@Autowired
 	private PostRepository repo;
 	
@@ -19,15 +17,8 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	/*
-	public Post findById(String id) {
-		Post user = repo.findOne(id);
-		if (user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");
-		}
-		return user;
-	}
-	*/
 
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
